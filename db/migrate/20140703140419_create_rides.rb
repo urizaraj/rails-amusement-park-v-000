b5f41cfb830c54e58590a338b83852e19d255570
Alexander Urizar <urizaraj@gmail.com>
2018-03-19 18:50:35 +0000

    Done.

--- db/migrate/20140703140419_create_rides.rb
+++ db/migrate/20140703140419_create_rides.rb
@@ -1,3 +1,8 @@
 class CreateRides < ActiveRecord::Migration
-  # Write your migrations here
+  def change
+    create_table :rides do |t|
+      t.integer :attraction_id
+      t.integer :user_id
+    end
+  end
 end
