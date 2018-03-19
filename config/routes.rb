8872463f04e7b67193a09457687f6d1a9c60cf67
Alexander Urizar <urizaraj@gmail.com>
2018-03-19 19:41:20 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :users
 
+  root 'static#home'
 
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
