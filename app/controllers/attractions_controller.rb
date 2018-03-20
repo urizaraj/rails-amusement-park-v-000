fab46cd6441ebb21f7cc9e2312dbc7ab12837ae4
Alexander Urizar <urizaraj@gmail.com>
2018-03-20 16:44:11 +0000

    Done.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,7 +1,8 @@
 class AttractionsController < ApplicationController
-  before_action :set_attraction, only: %i[show go_on_attraction]
+  before_action :set_attraction, only: %i[show go_on_attraction edit update]
 
   def index
+    @prefix = current_user.admin ? 'Show' : 'Go on'
     @attractions = Attraction.all
   end
 
@@ -14,8 +15,27 @@ class AttractionsController < ApplicationController
     redirect_to current_user
   end
 
+  def new
+    @attraction = Attraction.new
+  end
+
+  def create
+    attraction = Attraction.new(strong_params)
+    return render :new unless attraction.save
+    redirect_to attraction
+  end
+
+  def update
+    @attraction.update(strong_params)
+    redirect_to @attraction
+  end
+
   private
   def set_attraction
     @attraction = Attraction.find(params[:id])
+  end
+
+  def strong_params
+    params.require(:attraction).permit(*%i[name nausea_rating happiness_rating tickets min_height])
   end
 end
